be64df0a4ef6647d779b155f75c48e9a5427e9b3
ADITYA08-a <adithyakrishnavijay@gmail.com>
2026-07-07 23:51:34 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import os, cv2, rasterio, torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+from skimage.filters import gaussian
+
+class U_Net(nn..Module):
+    def __init__(self):
+        pass
